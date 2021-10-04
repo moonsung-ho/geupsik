2d3c02c3add3e86c6bfe312719180675b6d1132e
sungho0205 <moon@7959.ml>
2021-10-04 11:01:58 +0900

    컴퓨터만 console에 NASA 사진 표시

--- printgeupsik.mjs
+++ printgeupsik.mjs
@@ -1,12 +1,21 @@
 const printGeupsik = () => {
-  fetch(
-    'https://api.nasa.gov/planetary/apod?thumbs=True&api_key=2ytHG6k3cfyHLcTdGUhpaedPMb17a0V2DQd5EogF',
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(`오늘자 NASA 선정 사진: ${json.title}`);
-      console.log('%c ', `font-size:300px; background:url(${json.hdurl}) no-repeat;`);
-      console.log(json.explanation)
-    });
+  if (
+    ['Win16', 'Win32', 'Win64', 'Mac', 'MacIntel'].find(
+      (element) => element == navigator.platform,
+    )
+  ) {
+    fetch(
+      'https://api.nasa.gov/planetary/apod?thumbs=True&api_key=2ytHG6k3cfyHLcTdGUhpaedPMb17a0V2DQd5EogF',
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(`오늘자 NASA 선정 사진: ${json.title}`);
+        console.log(
+          '%c ',
+          `font-size:300px; background:url(${json.hdurl}) no-repeat;`,
+        );
+        console.log(json.explanation);
+      });
+  }
 };
 export { printGeupsik };
