5c24220d92d06ab639df7aaf0bf68cdda56e010e
sungho0205 <moon@7959.ml>
2021-10-11 12:50:48 +0900

    ios 배너 추가

--- app.js
+++ app.js
@@ -76,8 +76,25 @@ const isInStandaloneMode = () =>
   'standalone' in window.navigator && window.navigator.standalone;
 
 // Checks if should display install popup notification:
-if (isIos() && !isInStandaloneMode()) {
-  this.setState({ showInstallMessage: true });
+if (isIos() && !isInStandaloneMode() && localStorage.getItem("homescreenbanner") !== "true") {
+  localStorage.setItem("homescreenbanner", "true")
+  document.getElementsByTagName('footer')[0].innerHTML = `${
+    document.getElementsByTagName('footer')[0].innerHTML
+  }<style>
+  @keyframes colorChange{
+    0%  { color: #8BC34A;}
+    25%  { color: #03A9F4;}
+    50%  { color: #FF5722;}
+    100%{ color: #607D8B;}
+  }
+  .descAnim765 {
+    animation: colorChange 1s ease-out 0s infinite alternate none running;
+  }
+  </style>
+  <div  style="margin: 200 auto;text-align:center;overflow:hidden;border-radius:0px;background:#ffffff;border:0px solid #ff0000;padding:8px;max-width:calc(100% - 16px);width:700px">
+    <div  style="display:inline-block;position:relative;vertical-align: middle;padding:3px;font-size:45px;color:#000000;font-weight:normal">앱을 홈 화면에 <br>추가하세요!</div>
+    <div class="descAnim765"  style="display:inline-block;position:relative;vertical-align: middle;padding:3px;font-size:14px;color:#000000;font-weight:bold">공유 버튼을 누르고 홈 화면에 추가 버튼을 누르세요!</div>
+  </div>`;
 }
 
 let geupsik = localStorage.getItem('geupsik');
