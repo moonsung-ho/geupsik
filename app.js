3f9e905e21d1d1fe8e66c1e7a95d3daf0efac82d
sungho0205 <moon@7959.ml>
2021-10-12 17:40:46 +0900

    d

--- app.js
+++ app.js
@@ -76,8 +76,12 @@ const isInStandaloneMode = () =>
   'standalone' in window.navigator && window.navigator.standalone;
 
 // Checks if should display install popup notification:
-if (isIos() && !isInStandaloneMode() && localStorage.getItem("homescreenbanner") !== "true") {
-  localStorage.setItem("homescreenbanner", "true")
+if (
+  isIos() &&
+  !isInStandaloneMode() &&
+  localStorage.getItem('homescreenbanner') !== 'true'
+) {
+  localStorage.setItem('homescreenbanner', 'true');
   document.getElementsByTagName('footer')[0].innerHTML = `${
     document.getElementsByTagName('footer')[0].innerHTML
   }<style>
@@ -91,9 +95,9 @@ if (isIos() && !isInStandaloneMode() && localStorage.getItem("homescreenbanner")
     animation: colorChange 1s ease-out 0s infinite alternate none running;
   }
   </style>
-  <div  style="margin: 200 auto;text-align:center;overflow:hidden;border-radius:0px;background:#ffffff;border:0px solid #ff0000;padding:8px;max-width:calc(100% - 16px);width:700px">
-    <div  style="display:inline-block;position:relative;vertical-align: middle;padding:3px;font-size:45px;color:#000000;font-weight:normal">앱을 홈 화면에 <br>추가하세요!</div>
-    <div class="descAnim765"  style="display:inline-block;position:relative;vertical-align: middle;padding:3px;font-size:14px;color:#000000;font-weight:bold">공유 버튼을 누르고 홈 화면에 추가 버튼을 누르세요!</div>
+  <div style="margin: 200 auto;text-align:center;overflow:hidden;border-radius:0px;border:0px solid #ff0000;padding:8px;max-width:calc(100% - 16px);width:700px" height:30px;>
+    <div class="descAnim765" style="height:20px; display:inline-block;position:relative;vertical-align: middle;padding:3px;font-size:20px;color:#000000;font-weight:normal">앱을 홈 화면에 추가하세요!</div>
+    <div class="descAnim765"  style="display:inline-block;position:relative;vertical-align: middle;padding:3px;font-size:11px;color:#000000;font-weight:bold">공유 버튼을 누르고 홈 화면에 추가 버튼을 누르세요!</div>
   </div>`;
 }
 
