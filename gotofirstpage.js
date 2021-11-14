2eab83ea2cb175dbbaa03a5a347db0a7ec61aeac
Sungho Moon <moon7959@kakao.com>
2021-11-14 10:21:23 +0900

    d

--- gotofirstpage.js
+++ gotofirstpage.js
@@ -1,3 +1,3 @@
-if (localStorage.getItem("schoolcode") === null && Object.fromEntries(new URLSearchParams(window.location.search).entries()).toString === "[object Object]") {
+if (localStorage.getItem("schoolcode") === null && Object.fromEntries(new URLSearchParams(window.location.search).entries()).date) {
     location.href = "/first";
 }
