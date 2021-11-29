e7ea83188897352bc573bd91166e3f2c6b590fad
Sungho Moon <moon7959@kakao.com>
2021-11-29 21:32:09 +0900

    처음 들어간 사용자는 스와이프로 날짜 전환

--- js/start.js
+++ js/start.js
@@ -7,7 +7,20 @@ import { getOfficeQuery, getSchoolQuery, getDateQuery } from '/js/getQuery.js';
 export let schoolCode = getSchoolCode();
 export let officeCode = localStorage.getItem('officecode');
 
-
+if (!localStorage.getItem('officecode')) {
+  Toastify({
+    text: "스와이프로 날짜를 전환할 수 있습니다!",
+    duration: 3000,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "center", // `left`, `center` or `right`
+    stopOnFocus: false, // Prevents dismissing of toast on hover
+    style: {
+      background: "wheat",
+    },
+    onClick: function () { } // Callback after click
+  }).showToast();
+}
 
 if (document.querySelector("html").className === "dark") {
   document.querySelector("#github").innerHTML = '<a href="https://github.com/sungho0205/geupsik"><img src="images/github.png" alt="깃허브"></a>'
