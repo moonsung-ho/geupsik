3e28c3c93565006ebc45f624947e08a5579994c4
문성호 <sungho@munseonghoui-MacBookPro.local>
2022-03-09 09:04:26 +0900

    d

--- js/start.js
+++ js/start.js
@@ -1,11 +1,11 @@
-import { getSchoolCode } from '/js/localstorage.mjs';
-import { getDateStr, parseDateStr } from '/js/date_utilities.mjs';
-import { getMealInfo } from '/js/api.mjs';
-import { printNASAPicture } from './printNASAPicture.mjs';
-import { getOfficeQuery, getSchoolQuery, getDateQuery } from '/js/getQuery.js';
+import { getSchoolCode } from "/js/localstorage.mjs";
+import { getDateStr, parseDateStr } from "/js/date_utilities.mjs";
+import { getMealInfo } from "/js/api.mjs";
+import { printNASAPicture } from "./printNASAPicture.mjs";
+import { getOfficeQuery, getSchoolQuery, getDateQuery } from "/js/getQuery.js";
 
 if (!localStorage.getItem("experimental_beta")) {
-  if ((Math.random() * (10 - 1) + 1) === 2) {
+  if (Math.random() * (10 - 1) + 1 === 2) {
     Toastify({
       text: "실험적 베타 기능 대상입니다.",
       duration: 3000,
@@ -14,9 +14,9 @@ if (!localStorage.getItem("experimental_beta")) {
       position: "center", // `left`, `center` or `right`
       stopOnFocus: false, // Prevents dismissing of toast on hover
       style: {
-        background: "wheat",
+        background: "wheat"
       },
-      onClick: function () { } // Callback after click
+      onClick: function () {} // Callback after click
     }).showToast();
     localStorage.setItem("experimental_beta", "true");
   } else {
@@ -24,15 +24,15 @@ if (!localStorage.getItem("experimental_beta")) {
   }
 }
 
-if (localStorage.getItem('schoolcode')) {
+if (localStorage.getItem("schoolcode")) {
   var schoolCode = getSchoolCode();
-  var officeCode = localStorage.getItem('officecode');
+  var officeCode = localStorage.getItem("officecode");
 } else {
-  var schoolCode = sessionStorage.getItem('schoolcode');
-  var officeCode = sessionStorage.getItem('officecode');
+  var schoolCode = sessionStorage.getItem("schoolcode");
+  var officeCode = sessionStorage.getItem("officecode");
 }
 
-if (!localStorage.getItem('geupsik')) {
+if (!localStorage.getItem("geupsik")) {
   Toastify({
     text: "스와이프 또는 방향키로 날짜를 전환할 수 있습니다!",
     duration: 3000,
@@ -41,42 +41,46 @@ if (!localStorage.getItem('geupsik')) {
     position: "center", // `left`, `center` or `right`
     stopOnFocus: false, // Prevents dismissing of toast on hover
     style: {
-      background: "wheat",
+      background: "wheat"
     },
-    onClick: function () { } // Callback after click
+    onClick: function () {} // Callback after click
   }).showToast();
 }
 
 printNASAPicture();
 
 //url params
-export let dateInput = document.querySelector('#select-date');
+export let dateInput = document.querySelector("#select-date");
 const urlSearchParams = new URLSearchParams(window.location.search);
 export const params = Object.fromEntries(urlSearchParams.entries());
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js");
 }
 
 dateInput.value = getDateStr();
 
-let geupsik = localStorage.getItem('geupsik');
-localStorage.setItem('geupsik', geupsik * 1 + 1);
+let geupsik = localStorage.getItem("geupsik");
+localStorage.setItem("geupsik", geupsik * 1 + 1);
 
-document.querySelector('#select-date').onchange = function () {
+document.querySelector("#select-date").onchange = function () {
   let dayChosen = parseDateStr(dateInput.value);
   getMealInfo(schoolCode, officeCode, dayChosen);
-  window.history.pushState({}, document.title, `?date=${dateInput.value}&schoolcode=${schoolCode}&officecode=${officeCode}`);
+  window.history.pushState(
+    {},
+    document.title,
+    `?date=${dateInput.value}&schoolcode=${schoolCode}&officecode=${officeCode}`
+  );
 };
 
 getMealInfo(getSchoolQuery(), getOfficeQuery(), getDateQuery());
 
 if (schoolCode && officeCode) {
-  sessionStorage.setItem('schoolcode', schoolCode);
-  sessionStorage.setItem('officecode', officeCode);
+  sessionStorage.setItem("schoolcode", schoolCode);
+  sessionStorage.setItem("officecode", officeCode);
 } else {
-  sessionStorage.setItem('schoolcode', getSchoolQuery());
-  sessionStorage.setItem('officecode', getOfficeQuery());
+  sessionStorage.setItem("schoolcode", getSchoolQuery());
+  sessionStorage.setItem("officecode", getOfficeQuery());
 }
 
 export { schoolCode, officeCode };
